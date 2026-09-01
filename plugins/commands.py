555caf778a600152b436161b73e277c0ad8cacf7
ravikantsharma925490-design <ravikantsharma925490@gmail.com>
2026-09-01 10:16:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -82,8 +82,7 @@ async def start(client, message):
         return         
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
-                    InlineKeyboardButton('❤️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ❤️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-                ],[
+                   
                     InlineKeyboardButton('🍁 Update Channel 🍁', url=UPDATE_CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
