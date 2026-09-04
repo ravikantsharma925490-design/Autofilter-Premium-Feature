4b24340cb599aeab16fc5cd8d2e4760062cdeb13
ravikantsharma925490-design <ravikantsharma925490@gmail.com>
2026-09-04 15:25:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -97,7 +97,7 @@ async def start(client, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
- buttons = [[
+
    [
     InlineKeyboardButton(
         '📢 ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 📢',
