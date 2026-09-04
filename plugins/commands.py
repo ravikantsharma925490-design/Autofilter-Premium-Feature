f671cc7f12a3b33cc53b36d60acb3e7c6a153db0
ravikantsharma925490-design <ravikantsharma925490@gmail.com>
2026-09-04 16:10:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -98,14 +98,16 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [[
-                    InlineKeyboardButton( '📢 ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 📢',
-        url='https://t.me/cinemagyanupdates')
-             InlineKeyboardButton(
-        '👥 ᴊᴏɪɴ ɢʀᴏᴜᴘ 👥',
-        url='https://t.me/english_chatting_group18'
-    )
-                ],[
+            buttons = [[
+        InlineKeyboardButton(
+            '📢 ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 📢',
+            url='https://t.me/cinemagyanupdates'
+        ),
+        InlineKeyboardButton(
+            '👥 ᴊᴏɪɴ ɢʀᴏᴜᴘ 👥',
+            url='https://t.me/english_chatting_group18'
+        )
+    ],[
                     InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
                     InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
                 ],[
