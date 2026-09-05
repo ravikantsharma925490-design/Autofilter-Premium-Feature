ed8b7cd2b5f54e22bff4829bc479d2fea37b39a3
ravikantsharma925490-design <ravikantsharma925490@gmail.com>
2026-09-05 13:07:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/')  # Support group lin
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-100")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
 auth_channels     = environ.get("AUTH_CHANNELS", "-100")# Channels for force sub (make sure bot is admin)
-
+auth_groups = environ.get("AUTH_GROUPS", "-100")
 # ============================
 # Payment Configuration
 # ============================
